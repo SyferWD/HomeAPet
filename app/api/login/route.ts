33f97f52933fa770b1f48059394afad5d6185de0
AlvinWongCK <88760016+AlvinWongCK@users.noreply.github.com>
2023-08-26 20:59:35 +0800

    updated models and api

---
+++ app/api/login/route.ts
@@ -0,0 +1,28 @@
+import { prisma } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async ( req: Request, res: Response) => {
+    try {
+        const allUsers = await prisma.user.findMany()
+        return NextResponse.json({ message: "Success: ", allUsers}, {status: 200})
+    } catch (error) {
+        return NextResponse.json({ message: "Error", error}, {status: 500})
+    }
+};
+
+export const POST = async (req: Request) => {
+
+    try {
+        console.log("test");
+        const newUser = await prisma.user.create({
+            data: {
+                username: 'test1',
+                email: 'test2@test.com',
+                password: '123456',
+            }
+        });
+        return NextResponse.json({message: "Success: ", newUser}, {status: 201})
+    } catch (error) {
+        return NextResponse.json({message: "Error" , error}, {status: 500})
+    }
+}
